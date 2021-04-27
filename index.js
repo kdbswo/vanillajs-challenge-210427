9e5ae1a1c289bbf3a601fc63b02cbf59b63e17cf
kdbswo <75184363+kdbswo@users.noreply.github.com>
2021-04-28 02:58:02 +0900

    add push btn

--- index.js
+++ index.js
@@ -13,12 +13,56 @@ function savePending (){
     localStorage.setItem(PENDING_LS, JSON.stringify(pending))
 }
 
+function saveFinished(){
+    localStorage.setItem(FINISHED_LS, JSON.stringify(finished))
+}
+
+function deletePend (event){
+    const pendBtn = event.target;
+    const li = pendBtn.parentNode;
+    pendingList.removeChild(li);
+    const cleanPend =  pending.filter(function(toDo){
+        return toDo.id !== parseInt(li.id);
+    });
+    pending = cleanPend
+    savePending();
+}
+
+function pushPend (event) {
+    const pendBtn = event.target;
+    const parent = pendBtn.parentNode;
+    const addId = parent.id;
+    const br = pendBtn.previousSibling;
+    const addspan = br.previousSibling;
+    const spanText = addspan.innerHTML;
+    const li = document.createElement("li");
+    const span = document.createElement("span")
+    span.innerText = spanText;
+    const obj = {
+        text: spanText,
+        id: addId
+    };
+    li.append(span);
+    finishedList.appendChild(li);
+    finished.push(obj);
+    savePending();
+    saveFinished();
+}
+
 function paintToDo(text){
     const li = document.createElement("li");
     const span = document.createElement("span");
+    const delPend = document.createElement("button")
+    const pushPendBtn = document.createElement("button")
     const newId = pending.length + 1;
+    delPend.innerText = "❌";
+    delPend.addEventListener("click", deletePend)
+    pushPendBtn.innerText = "✔";
+    pushPendBtn.addEventListener("click",pushPend)
     span.innerText = text;
     li.appendChild(span);
+    li.appendChild(delPend);
+    li.appendChild(pushPendBtn);
     li.id = newId;
     pendingList.appendChild(li);
     const obj = {
@@ -36,7 +80,19 @@ function handleSubmit (event) {
     input.value = "";
 }
 
+function loadList (){
+    const loadedPending = localStorage.getItem(PENDING_LS);
+    const loadedFinished = localStorage.getItem(FINISHED_LS);
+    if (loadedPending !== null) {
+        const parsedPending = JSON.parse(loadedPending);
+        parsedPending.forEach (function(toDo){
+            paintToDo(toDo.text);
+        })
+    }
+}
+
 function init (){
+    loadList();
     form.addEventListener("submit", handleSubmit);
 }
 
